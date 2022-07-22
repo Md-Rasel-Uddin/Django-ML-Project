ab5a7890c9740c26f777614b75e18833daf2c4c8
Md Rasel Uddin <raseluddin102@gmail.com>
2022-07-23 02:32:46 +0600

    Update urls.py

--- disease_prediction/urls.py
+++ disease_prediction/urls.py
@@ -20,5 +20,8 @@ urlpatterns = [
      path('admin/', admin.site.urls),
      path("", include("main_app.urls")),
      path("accounts/", include("accounts.urls")),
-     path("", include("chats.urls"))
+     path("", include("chats.urls")),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
